f31e42d3c7a6dbb8ab3197c7445739fca986ac15
Juliana Quintas <julianasofiaquintas@gmailcom>
2026-03-13 15:36:08 +0000

    commit add env ref

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { Tasklist } from './components/tasklist/tasklist';
 import { Options } from './components/options/options';
 import { Dashboard } from './components/dashboard/dashboard';
+import { environment } from '../environments/environment.development';
+
 
 @Component({
   selector: 'app-root',
@@ -21,4 +23,14 @@ export class App {
   receber(nome:string) {
     alert("Utilizador selecionado: " + nome);
   }
+
 }
+async function loadUsers () {
+  const response = await fetch (environment.apiUrl + '/users');
+  const users = await response.json();
+
+  console.log(users)
+
+}
+
+loadUsers()
